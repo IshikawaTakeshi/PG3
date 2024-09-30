32e0b1ed05704f06850452bb7bbc2257fc481e14
IshikawaTakeshi <146906463+IshikawaTakeshi@users.noreply.github.com>
2024-09-30 11:34:49 +0900

    関数テンプレートMinを作成した

---
+++ project/main.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+template <typename T>
+T Min(T a, T b) {
+	return static_cast<T>(a < b ? a : b);
+}
+
+template <>
+char Min<char>(char a, char b) {
+	return printf("数字以外は代入できません\n");
+}
+
+
+
+int main() {
+
+	printf("int    : %d\n",Min<int>(114,514));
+	printf("float  : %f\n",Min<float>(11.4f,51.4f));
+	printf("double : %lf\n", Min<double>(1.14, 5.14));
+	printf("%c\n", Min<char>('a', 'b'));
+
+	return 0;
+}
