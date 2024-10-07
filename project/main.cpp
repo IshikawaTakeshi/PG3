fdeffb4677702507f3bff0301a9328285bc5e285
IshikawaTakeshi <146906463+IshikawaTakeshi@users.noreply.github.com>
2024-10-07 10:52:22 +0900

    マークダウン追加

--- project/main.cpp
+++ project/main.cpp
@@ -1,23 +1,46 @@
 #include <stdio.h>
 
+
+int RecursiveWage(int time,int wage) {
+	if (time <= 1) return wage;
+	int newWage = wage;
+	newWage = (newWage * 2 - 50);
+	return RecursiveWage(time - 1, newWage);
+}
+
 template <typename T>
-T Min(T a, T b) {
-	return static_cast<T>(a < b ? a : b);
+T Max(T a, T b) {
+	return a > b ? a : b;
 }
-
-template <>
-char Min<char>(char a, char b) {
-	return printf("数字以外は代入できません\n");
-}
-
 
 
 int main() {
+	//一般的な賃金体系
+	int hourlyWage = 1072;
+	//再帰的な賃金体系
+	int recursiveWage = 100;
+	//労働時間
+	int workingHours = 0;
 
-	printf("int    : %d\n",Min<int>(114,514));
-	printf("float  : %f\n",Min<float>(11.4f,51.4f));
-	printf("double : %lf\n", Min<double>(1.14, 5.14));
-	printf("%c\n", Min<char>('a', 'b'));
+	//労働時間を入力
+	printf("労働時間を入力してください: ");
+	scanf_s("%d", &workingHours);
+	printf("労働時間 : %d時間\n", workingHours);
 
+	//一般的な賃金体系の計算
+	int generalWage = hourlyWage * workingHours;
+	//再帰的な賃金体系の計算
+	int result = 0;
+	for (int i = 1; i <= workingHours; i++) {
+		int newWage = RecursiveWage(i, recursiveWage);
+		result += newWage;
+	}
+	
+
+	//結果の出力
+	printf("一般的な賃金体系で%d時間働いた: %d\n",workingHours, generalWage);
+	printf("再帰的な賃金体系で%d時間働いた: %d\n",workingHours, result);
+	printf("収入が高い方: %d\n", Max(generalWage, result));
+	
 	return 0;
 }
