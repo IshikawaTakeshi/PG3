97add17880fa3df00ec0fc9a34bb3fd8918293e5
IshikawaTakeshi <146906463+IshikawaTakeshi@users.noreply.github.com>
2024-10-28 11:37:34 +0900

    使っていない関数の削除

--- project/main.cpp
+++ project/main.cpp
@@ -5,29 +5,6 @@
 
 #include "Enemy.h"
 
-typedef void (*FuncPtr)(int*);
-
-//乱数の生成
-int Random(int min, int max) {
-	std::random_device rd;
-	std::mt19937 mt(rd());
-	return std::uniform_int_distribution<int>(min, max)(mt);
-}
-
-//待ち時間の設定
-void SetTimeOut(int count) {
-	for (int i = 0; i < count; i++) {
-		putchar('.');
-		Sleep(1000); //ミリ秒単位
-	}
-}
-
-void Answer(int* answer) {
-	printf("回答 : ");
-	scanf_s(" %d", answer);
-}
-
-
 int main(void) {
 	Enemy* enemy = new Enemy();
 	enemy->Update();
