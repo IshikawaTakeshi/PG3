bc11407eff666f35decefa048d232dfd51379a43
IshikawaTakeshi <146906463+IshikawaTakeshi@users.noreply.github.com>
2024-10-28 12:01:11 +0900

    Enemyクラスの削除

--- project/main.cpp
+++ project/main.cpp
@@ -3,26 +3,12 @@
 #include <random>
 #include <functional>
 
-template <typename T>
-class MathUtil {
-public:
 
-	T a;
-	T b;
-
-	MathUtil(T a, T b) : a(a), b(b) {}
-	
-	T Min() {
-		return static_cast<T>(a < b ? a : b);
-	}
-};
+class I;
 
 int main(void) {
 
-	MathUtil<int> mathUtil(1, 2);
-	MathUtil<float> mathUtil2(1.0f, 2.0f);
-	MathUtil<double> mathUtil3(1.0, 2.0);
-
+	
 	printf("int min: %d\n", mathUtil.Min());
 	printf("float min: %f\n", mathUtil2.Min());
 	printf("double min: %f\n", mathUtil3.Min());
