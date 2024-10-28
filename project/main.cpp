e2b1c9b1ed2a28cfd65a9e16c1b670ece72a0b75
IshikawaTakeshi <146906463+IshikawaTakeshi@users.noreply.github.com>
2024-10-28 11:55:18 +0900

    クラスMathUtilの作成 メンバ関数Minの実装

--- project/main.cpp
+++ project/main.cpp
@@ -3,12 +3,29 @@
 #include <random>
 #include <functional>
 
-#include "Enemy.h"
+template <typename T>
+class MathUtil {
+public:
 
+	T a;
+	T b;
+
+	MathUtil(T a, T b) : a(a), b(b) {}
+	
+	T Min() {
+		return static_cast<T>(a < b ? a : b);
+	}
+};
 
 int main(void) {
-	Enemy* enemy = new Enemy();
-	enemy->Update();
+
+	MathUtil<int> mathUtil(1, 2);
+	MathUtil<float> mathUtil2(1.0f, 2.0f);
+	MathUtil<double> mathUtil3(1.0, 2.0);
+
+	printf("int min: %d\n", mathUtil.Min());
+	printf("float min: %f\n", mathUtil2.Min());
+	printf("double min: %f\n", mathUtil3.Min());
 
 	return 0;
 }
