ee6e3d51f151dedc2e591d393ce2cff04f70200a
IshikawaTakeshi <146906463+IshikawaTakeshi@users.noreply.github.com>
2024-10-28 12:30:54 +0900

    基底クラスISodaの作成 派生クラスCocaCola,Pepsi,Fantaの作成

--- project/main.cpp
+++ project/main.cpp
@@ -4,14 +4,43 @@
 #include <functional>
 
 
-class I;
+class ISoda {
+public:
+	virtual void PrintName() = 0;
+
+};
+
+class CocaCola : public ISoda {
+public:
+	void PrintName() override {
+		printf("CocaCola is Soda.\n");
+	}
+};
+
+class Pepsi : public ISoda {
+
+public:
+	void PrintName() override {
+		printf("PepsiCola is Soda.\n");
+	}
+};
+
+class Fanta : public ISoda {
+public:
+	void PrintName() override {
+		printf("Fanta is Soda.\n");
+	}
+};
 
 int main(void) {
 
-	
-	printf("int min: %d\n", mathUtil.Min());
-	printf("float min: %f\n", mathUtil2.Min());
-	printf("double min: %f\n", mathUtil3.Min());
+	ISoda* pSoda = new CocaCola();
+	ISoda* pSoda2 = new Pepsi();
+	ISoda* pSoda3 = new Fanta();
+
+	pSoda->PrintName();
+	pSoda2->PrintName();
+	pSoda3->PrintName();
 
 	return 0;
 }
