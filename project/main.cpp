b427626d128f22f513419b403b3e8d9ca875a8c8
IshikawaTakeshi <146906463+IshikawaTakeshi@users.noreply.github.com>
2024-10-28 12:05:33 +0900

    各型の組み合わせたインスタンスの作成

--- project/main.cpp
+++ project/main.cpp
@@ -3,29 +3,35 @@
 #include <random>
 #include <functional>
 
-template <typename T>
+template <typename T1,typename T2>
 class MathUtil {
 public:
 
-	T a;
-	T b;
+	T1 a;
+	T2 b;
 
-	MathUtil(T a, T b) : a(a), b(b) {}
+	MathUtil(T1 a, T2 b) : a(a), b(b) {}
 	
-	T Min() {
-		return static_cast<T>(a < b ? a : b);
+	T1 Min() {
+		return static_cast<T1>(a < b ? a : b);
 	}
 };
 
 int main(void) {
 
-	MathUtil<int> mathUtil(1, 2);
-	MathUtil<float> mathUtil2(1.0f, 2.0f);
-	MathUtil<double> mathUtil3(1.0, 2.0);
+	MathUtil<int,int> mathUtil(1, 2);
+	MathUtil<float,float> mathUtil2(1.0f, 2.0f);
+	MathUtil<double,double> mathUtil3(1.0, 2.0);
+	MathUtil<int, float> mathUtil4(1, 2.0f);
+	MathUtil<float, double> mathUtil5(1.0f, 2.0);
+	MathUtil<double, int> mathUtil6(1.0, 2);
 
 	printf("int min: %d\n", mathUtil.Min());
 	printf("float min: %f\n", mathUtil2.Min());
 	printf("double min: %f\n", mathUtil3.Min());
+	printf("int, float min: %d\n", mathUtil4.Min());
+	printf("float, double min: %f\n", mathUtil5.Min());
+	printf("double, int min: %f\n", mathUtil6.Min());
 
 	return 0;
 }
