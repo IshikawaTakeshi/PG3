0a3f0d6b116fa1024f6517ff2845660e77412e5c
IshikawaTakeshi <146906463+IshikawaTakeshi@users.noreply.github.com>
2024-11-18 11:38:17 +0900

    山手線の駅一覧のリストを作った

--- project/main.cpp
+++ project/main.cpp
@@ -1,13 +1,121 @@
 #include <stdio.h>
 #include <Windows.h>
-#include <random>
-#include <functional>
+#include <list>
 
-#include "Enemy.h"
 
 int main(void) {
-	Enemy* enemy = new Enemy();
-	enemy->Update();
+
+	std::list<const char*> yamanoteLine2022 = {
+		"tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Nishi-Nippori",
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"Shin-Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		"Takanawa-Gateway",
+		"Tamachi",
+		"Hamamatsucho",
+		"Shimbashi",
+		"Yurakucho"
+	};
+
+	std::list<const char*> yamanoteLine2019{
+		"tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Nishi-Nippori",
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"Shin-Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		"Tamachi",
+		"Hamamatsucho",
+		"Shimbashi",
+		"Yurakucho"
+	};
+
+	std::list<const char*> yamanoteLine1970{
+		"tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimachi",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"Shin-Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		"Tamachi",
+		"Hamamatsucho",
+		"Shimbashi",
+		"Yurakucho"
+	};
+
+	// Display the Yamanote Line
+	printf("========== Yamanote Line 2022 ==========\n");
+	for (auto& station : yamanoteLine2022) {
+		printf("%s\n", station);
+	}
+
+	printf("\n==========Yamanote Line 2019 ==========\n");
+	for (auto& station : yamanoteLine2019) {
+		printf("%s\n", station);
+	}
+
+	printf("\n========== Yamanote Line 1970 ==========\n");
+	for (auto& station : yamanoteLine1970) {
+		printf("%s\n", station);
+	}
 
 	return 0;
 }
